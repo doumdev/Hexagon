57a32b119bc3c65580fb9e950f9ed232fd5e613b
Doum <doum@tuta.io>
2018-01-17 10:20:30 -0800

    v 0.01

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Hexagon developers
+//
+// This file is part of Hexagon.
+//
+// Hexagon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Hexagon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Hexagon.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid HEXAGON_NETWORK = { {  0x68, 0x65, 0x78, 0x61, 0x67, 0x6f, 0x6e, 0x1f, 0x34, 0x45, 0x00, 0x12, 0x10, 0x11, 0x01, 0x10  } };
+
+}
+
